5a7ee9ad04a94931db26a609d6a5175f37a8de9c
nataliaborgonos <natliabrgns@gmail.com>
2022-06-12 19:30:00 +0200

    añadido cargador videos

--- AppVideoJulio/src/main/java/dominio/Filtro.java
+++
@@ -1,8 +0,0 @@
-package dominio;
-
-import java.util.List;
-
-public abstract class Filtro {
-	//el video cumple los filtros
-		public abstract List<Video> esVideoOk(List<Video> listaVideos, Usuario usuario);
-}
